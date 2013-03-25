fe927e45bd432b92818cbcd0ace4ffecde7e76aa
tompng <tomoyapenguin@gmail.com>
2013-03-26 04:21:13 +0900

    split list-methods

--- lisp/methods_core.rb
+++ lisp/methods_core.rb
@@ -33,41 +33,11 @@ class LispEvaluator
         run c,hash
       end
     }
-    globals[:list]=->(hash,*args){
-      Quote.new Tree.list(*args.map{|x|run x,hash})
-    }
-    globals[:cons]=->(hash,a,b){
-      a=run(a,hash)
-      b=run(b,hash)
-      a=a.unquote if a.class==Quote
-      b=b.unquote if b.class==Quote
-      Quote.new Tree.new a,b
-    }
-    globals[:car]=->(hash,t){
-      tree=run(t,hash)
-      tree=tree.unquote if tree.class==Quote
-      if tree.left
-        Quote.new tree.left
-      end
-    }
-    globals[:cdr]=->(hash,t){
-      tree=run(t,hash)
-      tree=tree.unquote if tree.class==Quote
-      if tree.right
-        Quote.new tree.right
-      end
-    }
-    globals[:quote]=->(hash,t){
-      Quote.new t
-    }
     globals[:eval]=->(hash,code){
       run(run(code,hash).unquote,hash)
     }
     globals[:eq]=->(hash,a,b){
       run(a,hash)==run(b,hash)
-    }
-    globals[:atom]=->(hash,a){
-      run(a,hash).class!=Tree
     }
     globals[:p]=->(hash,*a){
       p *a.map{|x|run x,hash}
